1caaa4915c6893e52a2f0e6ab632221df58d1c09
separatrixxx <nikitalochmatov@gmail.com>
2024-04-04 13:15:11 +0300

    update contact info

--- helpers/get_contacts.tsx
+++ helpers/get_contacts.tsx
@@ -4,6 +4,6 @@ import { ContactsData } from "../interfaces/contacts.interface";
 export function getContactsData(): ContactsData {
     return {
         phone: '+995555169827',
-        email: 'banana.info@banana.codes',
+        email: 'contact@banana.codes',
     }
 }
